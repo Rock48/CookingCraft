eee2e26a91ee0b2667b2f09494c90a96b7d6f8ef
Rock48 <cquiglz@gmail.com>
2013-03-07 21:42:11 -0500

    Added redstone interaction

--- common/rock/minecraft/cc/block/BlockCakeSpawner.java
+++ common/rock/minecraft/cc/block/BlockCakeSpawner.java
@@ -20,11 +20,27 @@ public class BlockCakeSpawner extends Block
 	}
     public boolean onBlockActivated(World world, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
     {
-
    		world.setBlock(par2, par3+1, par4, Block.cake.blockID);
-    	return true;
-    	
+    	return true;	
     }
-    
-
+    public void onBlockAdded(World world, int par2, int par3, int par4)
+    {
+    	if(!world.isRemote)
+    	{
+            if(world.isBlockIndirectlyGettingPowered(par2, par3, par4)||world.isBlockGettingPowered(par2, par3, par4))
+            {
+           		world.setBlock(par2, par3+1, par4, Block.cake.blockID);
+            }
+    	}
+    }
+    public void onNeighborBlockChange(World world, int par2, int par3, int par4, int par5)
+    {	
+    	if(!world.isRemote)
+    	{
+            if(world.isBlockIndirectlyGettingPowered(par2, par3, par4)||world.isBlockGettingPowered(par2, par3, par4))
+            {
+           		world.setBlock(par2, par3+1, par4, Block.cake.blockID);
+            }
+    	}
+    }
 }
