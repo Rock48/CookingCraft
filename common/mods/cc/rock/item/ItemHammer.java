e89d0f28fe1d06ee5ab504a37a14bfc853b4f090
Rock48 <cquiglz@gmail.com>
2013-05-26 15:49:44 -0400

    I dare you to right click on creepers with the hammer

--- common/mods/cc/rock/item/ItemHammer.java
+++ common/mods/cc/rock/item/ItemHammer.java
@@ -2,12 +2,15 @@ package mods.cc.rock.item;
 
 import mods.cc.rock.CookingCraft;
 import mods.cc.rock.block.ModBlocks;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemHammer extends ItemCC{
+	World w;
 	public ItemHammer(int id, String texture) {
 		super(id, texture);
 		this.setUnlocalizedName("itemHammer")
@@ -20,6 +23,7 @@ public class ItemHammer extends ItemCC{
     {
     	if(!par3World.isRemote && (par3World.getBlockId(par4, par5, par6) == ModBlocks.machineCoreOn.blockID || par3World.getBlockId(par4, par5, par6) == ModBlocks.machineCoreOff.blockID))
     	{
+
     		par1ItemStack.damageItem(1, par2EntityPlayer);
 
     		return true;
@@ -27,7 +31,24 @@ public class ItemHammer extends ItemCC{
     	return false;
     }
     
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+		w = par2World;
+        return par1ItemStack;
+    }
     
+    public boolean itemInteractionForEntity(ItemStack par1ItemStack, EntityLiving par2EntityLiving){
+    	
+    	if(par2EntityLiving instanceof EntityCreeper && w != null){
+    		int tempHp = ((EntityCreeper) par2EntityLiving).getHealth();
+    		((EntityCreeper) par2EntityLiving).onStruckByLightning(new EntityLightningBolt(w, 0, 0, 0));
+    		((EntityCreeper) par2EntityLiving).extinguish();
+    		((EntityCreeper) par2EntityLiving).setEntityHealth(tempHp);
+    		return true;
+    	}
+    	
+    	return false;
+    }
 	
 	
 	
