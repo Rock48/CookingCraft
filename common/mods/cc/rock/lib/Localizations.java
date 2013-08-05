d53d698ebda5ae3194107fc1bfefc6866e965162
Rock48 <cquiglz@gmail.com>
2013-08-04 23:15:39 -0400

    fixed words

--- common/mods/cc/rock/lib/Localizations.java
+++ common/mods/cc/rock/lib/Localizations.java
@@ -3,7 +3,7 @@ package mods.cc.rock.lib;
 public class Localizations
 {
 
-    private static final String LANG_RESOURCE_LOCATION = "/mods/cc/lang/";
+    private static final String LANG_RESOURCE_LOCATION = "/assets/cc/lang/";
 
     public static String[] localeFiles = {LANG_RESOURCE_LOCATION + "en_US.xml"}; 
     
